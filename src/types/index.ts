2934a7892d57acd2c37b58e2e24bb65f1c621f88
petardjorovic <perodjorovic@gmail.com>
2026-01-24 20:03:09 +0100

    implement data provider

---
+++ src/types/index.ts
@@ -0,0 +1,10 @@
+import {number} from "zod";
+
+export type Subject = {
+    id: number;
+    name: string;
+    code: string;
+    description: string;
+    department: string;
+    createdAt: string;
+}
